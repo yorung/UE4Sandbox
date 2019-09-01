b391f283764ef2ab3c45d30d784d106f00489a55
yorung <yorung@gmail.com>
2019-09-01 23:16:03 +0900

    Add 'CrashTest' project.

---
+++ CrashTest/Source/CrashTest/CrashTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CrashTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRASHTEST_API ACrashTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
